1f96377f1981f38a78d323e0dda3e6ac94478519
Ana Crisan <acrisan@tableau.com>
2020-03-20 17:39:16 -0400

    adding details make modification

--- process_papers.R
+++ process_papers.R
@@ -3,7 +3,11 @@ library(adjutant)
 library(dplyr)
 library(ggplot2)
 
-dat<-read.csv(file="./data/all_sources_metadata_2020-03-13.csv",header=TRUE)
+#adding sha for docs that don't have it, makes life way better, could introduc some replicates though.
+dat<-read.csv(file="./data/all_sources_metadata_2020-03-13.csv",header=TRUE,stringsAsFactors = FALSE) %>%
+  mutate(sha= ifelse(sha == "", paste(sample(c(letters,0:9),52,replace=TRUE),collapse =""), sha))
+
+#files<- list.files(path = "./data/comm_use_subset/comm_use_subset/",full.names = TRUE)[1:500]
 
 files<- c(list.files(path = "./data/comm_use_subset/comm_use_subset/",full.names = TRUE),
           list.files(path = "./data/noncomm_use_subset/noncomm_use_subset/",full.names = TRUE),
@@ -42,6 +46,8 @@ for(i in 1:length(files)){
   res<-rbind(res,risResults)
 }
 
+save.image("intermediateAnalysis.Rda")
+
 #include and format papers that don't have full text
 no_txt<-dplyr::anti_join(dat, res, by=c("sha" = "PMID"))
 
@@ -60,12 +66,26 @@ no_txt_df<-data.frame(PMID=no_txt$sha,
                       stringsAsFactors = FALSE)
 
 #Convert the stuff that doesn't overlap
+#also making up for missing sha numbers
+#also, there are some duplicates, so just select the top one.
+#results is 17,399 unique articles.
+df <-rbind(res,no_txt_df) %>%
+  group_by(PMID) %>%
+  sample_n(1)%>%
+  ungroup()
 
+#remove those items that don't appear to have 
 
-df <-cbind(res,no_txt_df)
+#tidy corpus              
+tidy_df<-tidyCorpus(corpus = df,
+                    stopTerms = c("author","copyright","funder","holder−preprin",
+                                  "biorxiv", "copyright", "doi", "doi.org", "holder",
+                                  "http","peer","preprint","review",
+                                  "journal", "articl", "publi","includ", "studi"))
 
-tidy_df<-tidyCorpus(corpus = df) #tidy corpus
 tsneObj<-runTSNE(tidy_df,check_duplicates=FALSE)
+
+save.image("intermediateAnalysis.Rda")
 
 #add t-SNE co-ordinates to df object
 df<-inner_join(df,tsneObj$Y,by="PMID")
@@ -79,6 +99,10 @@ clustNames<-df %>%
   group_by(tsneCluster)%>%
   mutate(tsneClusterNames = getTopTerms(clustPMID = PMID,
                                         clustValue=tsneCluster,topNVal = 2,tidyCorpus=tidy_df)) %>%
+  mutate(tsneClusterNames = sapply(tsneClusterNames, function(x){
+    y<-strsplit(x,split="-")[[1]]
+    return(paste(y[1:2],collapse="-"))
+    }))%>%
   select(PMID,tsneClusterNames) %>%
   ungroup()
 
@@ -91,11 +115,15 @@ clusterNames <- df %>%
                    medY = median(tsneComp2)) %>%
   dplyr::filter(tsneClusterNames != "Not-Clustered")
 
-ggplot(df,aes(x=tsneComp1,y=tsneComp2,group=tsneClusterNames))+
+g<- ggplot(df,aes(x=tsneComp1,y=tsneComp2,group=tsneClusterNames))+
   geom_point(aes(colour = tsneClusterStatus),alpha=0.2)+
   stat_ellipse(aes(alpha=tsneClusterStatus))+
   geom_label(data=clusterNames,aes(x=medX,y=medY,label=tsneClusterNames),size=3,colour="red")+
   scale_colour_manual(values=c("black","blue"),name="cluster status")+
   scale_alpha_manual(values=c(1,0),name="cluster status")+ #remove the cluster for noise
   theme_bw()
+
+save.image("intermediateAnalysis.Rda")
+
+ggsave("alldata.pdf",g,height=15,width = 15, units="in")
 
---
+++ readme.md
@@ -0,0 +1,12 @@
+## COVID-19 Research Paper Analysis
+
+I had previosuly developed a method for unsupervised topic clustering called [Adjutant](https://github.com/amcrisan/Adjutant). THe purpose of Adjutant was to provide a quick, initial, set of clusters that can be used to orient an individual with a large document corpus. I have applied Adjutant here to the analysis of research papapers about COVID-19, which was [shared on kaggle](https://www.kaggle.com/allen-institute-for-ai/CORD-19-research-challenge).
+
+My goal is using Adjutant is to provide additional context to these researchers papers. 
+
+## How to use this data
+
+I am hoping the individuals that need better overviews of the data will find Adjutant's clusters to be of use on your analysis. I am also working on identifying clusters that could be aligne with kaggle's tasks.
+
+I've uploaded the reuslting clusters as a CSV file that augments the metadata file shipping with the kaggle data set. I am updating this information as dig into the data myself.
+
